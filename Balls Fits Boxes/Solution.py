58d78372b4f8aac86b41fa76a6f87608f01cb844
Satwik-RVS <95745517+Satwik-RVS@users.noreply.github.com>
2021-12-13 18:04:12 +0530

    Create Solution.py

---
+++ Balls Fits Boxes/Solution.py
@@ -0,0 +1,14 @@
+# RVS
+
+n = int(input())
+arr1 = list(map(int,input().split()))
+arr2 = list(map(int,input().split()))
+
+arr1.sort()
+arr2.sort()
+
+for i in range(n):
+    if arr1[i] <= arr2[i]:
+        print("NO!")
+        exit(0)
+print("YES")
