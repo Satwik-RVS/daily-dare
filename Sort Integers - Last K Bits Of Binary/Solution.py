dff55b6910e69d26aa1f7d3496f6d7472f77fc40
Satwik-RVS <95745517+Satwik-RVS@users.noreply.github.com>
2021-12-22 16:14:38 +0530

    Create Solution.py

---
+++ Sort Integers - Last K Bits Of Binary/Solution.py
@@ -0,0 +1,18 @@
+# RVS
+
+n = int(input())
+l = list(map(int,input().split()))
+k = int(input())
+
+d = {}
+for i in range(n):
+    x = l[i]
+    b = bin(x)[2:]
+    y = b[-2:]
+    if y not in d:
+        d[y] = []
+    d[y].append(x)
+
+x = sorted(d.keys())
+for i in x:
+    print(*sorted(d[i]),end=" ")
